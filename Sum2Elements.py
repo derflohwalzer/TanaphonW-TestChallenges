d0d4dca14127d3b01d145cfe7c0f1d052c339f92
derflohwalzer <tanaphon.wat@gmail.com>
2023-06-24 22:27:46 +0700

    Add comments and problem statement for each function

--- Sum2Elements.py
+++ Sum2Elements.py
@@ -1,13 +1,25 @@
-def Sum2Elements(nums, sum):
+def Sum2Elements(nums: list, sum: int):
+    """ 
+    Problem
+    Given an array of integers nums and an integer sum, 
+    return the indexes of 2 elements in the array that add up to the given sum.
+
+    Note
+    - There may be duplications in the array
+    - The array is not necessarily sorted
+    - The may be multiple pairs of elements that add up to the given sum. 
+    For simplicity, return only the first pair you can find
+    """
+
     length = len(nums)
 
     # idx1, idx2 = indexes for elements that add up to the sum
     for idx1 in range(length):
         diff = sum - nums[idx1]
         try:
-            idx2 = nums.index(diff, idx1 + 1)      #search from index inext  to
-            return (idx1,idx2)
+            idx2 = nums.index(diff, idx1 + 1)       # search from index after idx1, for num[idx2] == diff
+            return (idx1,idx2)                      # found matching pair, return output as a tuple
         except:
-            continue
+            continue                                # idx1 cannot match with any number to make sum, go to next idx1
     
-    return None
+    return None                                     # The are no two numbers that add up to sum
--- ThreeFive.py
+++ ThreeFive.py
@@ -1,4 +1,14 @@
 def ThreeFive():
+    
+    """ 
+    Problem
+    Write a function called ThreeFive to print number from 1 to 100 in a new line.
+        - if the number is divisible by 3 then print "Three"
+        - if the number is divisible by 5 then print "Five"
+        - if the number is divisible by both 3 and 5 then print "ThreeFive"
+        - Otherwise just print the number
+    """
+    
     for num in range(100):
         num = num + 1                       # begin at 1, not 0
         if num % 3 == 0 and num % 5 == 0:   # divisible by both 3 and 5
